0b6983c4be459d6df5d85a99eaabe2dd6ea8cd7d
vandafa <ifanszr@gmail.com>
2016-05-22 14:28:48 +0700

    Add files via upload

---
+++ defer5.js
@@ -0,0 +1,17 @@
+<script>
+
+function kafesia1() {
+var vidDefer = document.getElementsByTagName('iframe');
+for (var i=0; i<vidDefer.length; i++) {
+if(vidDefer[i].getAttribute('data-src')) {
+vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+} } }
+
+function kafesia2() {
+var imgDefer = document.getElementsByTagName('img');
+for (var i=0; i<imgDefer.length; i++) {
+if(imgDefer[i].getAttribute('data-src')) {
+imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+} } }
+
+</script>
