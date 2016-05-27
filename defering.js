e18e9910e42623d1152d66b6051f263d8e694ca5
vandafa <ifanszr@gmail.com>
2016-05-28 04:14:58 +0700

    Update defering.js

--- defering.js
+++ defering.js
@@ -1,7 +1,2 @@
-function kafesia2() {
-var imgDefer = document.getElementsByTagName('img');
-for (var i=0; i<imgDefer.length; i++) {
-if(imgDefer[i].getAttribute('data-src')) {
-imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-} } }
+var imgDefer = document.getElementsByTagName('img'); for (var i=0; i<imgDefer.length; i++) { if(imgDefer[i].getAttribute('data-src')) { imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src')); } }
 
